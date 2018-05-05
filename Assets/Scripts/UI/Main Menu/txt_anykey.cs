7d22127ca84f47b64f8bd29019a1ef0b942da216
BackRunner <liaoyp126@126.com>
2018-05-05 17:48:56 +0800

    add anims to second screen

---
+++ Assets/Scripts/UI/Main Menu/txt_anykey.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class txt_anykey : MonoBehaviour {
+
+    //obj
+    private GameObject img_logo;
+    private Animation anim_img_logo;
+
+    private void Start()
+    {
+        img_logo = transform.parent.Find("img_logo").gameObject;
+        anim_img_logo = img_logo.GetComponent<Animation>();
+    }
+
+    void Update () {
+		if (Input.anyKeyDown && !anim_img_logo.isPlaying)
+        {
+            anim_img_logo.Play("anim_logo_up");
+            //销毁防止二次触发
+            Destroy(gameObject);
+        }
+	}
+
+}
