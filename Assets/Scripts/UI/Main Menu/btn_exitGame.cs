8d145df8d3210cd86121399bc1039bd5d6631ac7
BackRunner <liaoyp126@126.com>
2018-05-06 21:43:49 +0800

    add select level elements

---
+++ Assets/Scripts/UI/Main Menu/btn_exitGame.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class btn_exitGame : MonoBehaviour {
+
+    private Button btn;
+
+    private void Awake()
+    {
+        btn = GetComponent<Button>();
+    }
+
+    private void Start()
+    {
+        //绑定事件
+        btn.onClick.AddListener(onClick);
+    }
+
+    public void onClick()
+    {
+        Debug.Log("Application Quit...");
+        UnityEditor.EditorApplication.isPlaying = false; //Editor
+        Application.Quit();
+    }
+}
