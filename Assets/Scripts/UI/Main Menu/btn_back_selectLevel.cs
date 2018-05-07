fa91577badef5770f2307fbf22cbd019e421f866
BackRunner <liaoyp126@126.com>
2018-05-07 21:00:56 +0800

    add anim to back btn

---
+++ Assets/Scripts/UI/Main Menu/btn_back_selectLevel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class btn_back_selectLevel : btn_back {
+
+    private GameObject panel;
+
+    public override void Start()
+    {
+        base.Start();
+        //初始化
+        if (gameController.panel_selectLevel_inscene != null)
+        {
+            panel = gameController.panel_selectLevel_inscene;
+        }
+    }
+
+    public override void Clicked()
+    {
+        Animation anim_mainmenu = mainmenu.GetComponent<Animation>();
+        anim_mainmenu.Play("anim_panel_mainmenu_in");
+        Animation anim_panel = panel.GetComponent<Animation>();
+        anim_panel.Play("anim_panel_selectLevel_out");
+        base.Clicked();
+    }
+
+}
