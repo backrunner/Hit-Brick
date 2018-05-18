18436726e1826eb37da49edf9c0d3577d7905e37
BackRunner <liaoyp126@126.com>
2018-05-18 09:47:35 +0800

    add logo screen

---
+++ Assets/Scripts/Game/logoScreenController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class logoScreenController : MonoBehaviour
+{
+
+    public GameObject canvas;
+    public GameObject panel_logo;
+    public static GameObject panel_logo_inscene;
+
+    void Start()
+    {
+        if (canvas == null)
+        {
+            canvas = GameObject.Find("Canvas");
+        }
+        DontDestroyOnLoad(canvas);
+        if (panel_logo != null)
+        {
+            panel_logo_inscene = Instantiate(panel_logo, canvas.transform);
+            Animation anim = panel_logo_inscene.GetComponent<Animation>();
+            anim.Play("anim_panel_logo");
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Anim/Ctrl/anim_ctrl_logo.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class anim_ctrl_logo : MonoBehaviour {
+
+    public string mainmenu_filename;
+    public int mainmenu_index;
+
+    public void animFinished()
+    {
+
+    }
+}
